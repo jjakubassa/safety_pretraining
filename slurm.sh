20c1cb3d41afef39ecfbc5f14b331eb51555ea1b
Jonas Jakubassa <jjakubas@slurm-submit-0.mpi-inf.mpg.de>
2025-07-01 00:27:37 +0200

    run more models

--- slurm.sh
+++ slurm.sh
@@ -8,13 +8,27 @@
 #SBATCH --mail-type=END,FAIL
 
 # setup
+# referecence model
 model1="HuggingFaceTB/SmolLM2-1.7B-Instruct"
-model1abl="HuggingFaceTB/SmolLM2-1.7B-Instruct-abliterated"
+model1abl="models/HuggingFaceTB/SmolLM2-1.7B-Instruct-abliterated"
+
+# only rephrasing no refusal
 model2="locuslab/mix_ift_v4-smollm2-1.7b-score0_mix_rephrased_from_beginning-600B"
 model2abl="models/locuslab/mix_ift_v4-smollm2-1.7b-score0_mix_rephrased_from_beginning-600B-abliterated"
+
+# one of the best models
 model3="locuslab/mix_ift_v9-smollm2-1.7b-score0_rephrase123_mild_ref45_metadata_5p-600B-metamix3p-1k-0"
-model3abl="locuslab/mix_ift_v9-smollm2-1.7b-score0_rephrase123_mild_ref45_metadata_5p-600B-metamix3p-1k-0-abliterated"
-abl_args="--scale-factor 1.0 --data-harmful data/harmful_train.parquet --data-harmless data/harmless_train.parquet"
+model3abl="models/locuslab/mix_ift_v9-smollm2-1.7b-score0_rephrase123_mild_ref45_metadata_5p-600B-metamix3p-1k-0-abliterated"
+
+# trained only on raw data
+# model4="locuslab/mix_ift_v9-smollm2-1.7b-all_raw_folders_meta-600B-metamix3p-1k-0"
+# model4abl="models/locuslab/mix_ift_v9-smollm2-1.7b-all_raw_folders_meta-600B-metamix3p-1k-0-abliterated"
+
+# second reference
+model5="mergekit-community/Qwen3-1.5B-Instruct"
+model5abl="models/mergekit-community/Qwen3-1.5B-Instruct-abliterated"
+
+abl_args="--data-harmful data/harmful_train.parquet --data-harmless data/harmless_train.parquet"
 
 echo $(date)
 
@@ -31,6 +45,14 @@ cmd="uv run abliterate.py -m $model3 -o $model3abl $abl_args"
 echo $cmd
 $cmd
 
+# cmd="uv run abliterate.py -m $model4 -o $model4abl $abl_args"
+# echo $cmd
+# $cmd
+
+cmd="uv run abliterate.py -m $model5 -o $model5abl $abl_args"
+echo $cmd
+$cmd
+
 # evaluate
-cmd="uv run evaluate.py --model_names $model1 $model1abl $model2 $model2abl $model3 $model3abl"
+cmd="uv run evaluate.py --model_names $model1 $model1abl $model2 $model2abl $model3 $model3abl  $model5 $model5abl" # $model4 $model4abl
 $cmd
